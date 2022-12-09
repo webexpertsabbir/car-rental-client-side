81affdf567ded8016949fe09eb105bf8bf39ad24
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-12-09 16:56:53 +0600

    frist commit

---
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>
+                this is a Navbar
+            </h2>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Router/Routes.js
@@ -0,0 +1,19 @@
+import Home from "../Pages/Home/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../layout/Main");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
